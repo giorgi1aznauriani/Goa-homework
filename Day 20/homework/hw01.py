269bdad0a0b8bd3ae6b5b9065f127ba0141dc741
giorgi1aznauriani <gio.2013gio@icloud.com>
2025-09-27 00:41:19 +0400

    second commit

--- Day 20/homework/hw01.py
+++ Day 20/homework/hw01.py
@@ -1,11 +1,11 @@
 # 2)მომხმარებელს შემოატანინე სახელი,თუ მომხმარებლის შემოტანილი სახელი შეიცავს ერთ uppercase ასოს მაინც(for loop გამოიყენეთ)მაშინ დაპრინტე ეს სახელი მთლიანად lowercase ში,სხვა შემთხვევაში დაპრინტე ეს სახელი რომელიც იქნება capitalize 
 
 name = input("Enter your name: ")
-contains_uppercase = False
+uppercase = False
 for i in name:
     if i.isupper():  
-        contains_uppercase = True
-if contains_uppercase:
+        uppercase = True
+if uppercase:
     print(name.lower())
 else:
     print(name.capitalize())
---
+++ Day 30/classwork/cw.py
@@ -0,0 +1,5 @@
+# შექმენით სეტი სადაც ჩაწერთ სახელებს და შემდეგ მომხმარებელს შემოატანინეთ კიდევ სახელიდ და დაამატეთ ჩვენს სეტში
+names = {"Giorgi", "Sandro", "mate",  }
+name1 = input("Enter your name: ")
+names.add(name1)
+print(names)
