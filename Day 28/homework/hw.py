5bb6caa292b0b34542dd8a5e206a6e2942fdb285
giorgi1aznauriani <gio.2013gio@icloud.com>
2025-09-27 19:14:48 +0400

    second commit

---
+++ Day 28/homework/hw.py
@@ -0,0 +1,5 @@
+# 2)შექმენით 5 ელემენტიანი Tuple და unpacking ის გამოყენებით თითოეულ ელემენტს მიანიჭეთ შესაბამისი ცვლადი
+
+cars = ("Mercedes", "Bmw", "Audi", "Rolls Royce", "Bugatti")
+
+name1,name2,name3,name4,name5=cars
