b191e18cf58ebf04308a39015ee3935d527ae8ff
giorgi1aznauriani <gio.2013gio@icloud.com>
2025-09-28 23:08:33 +0400

    second commit

---
+++ Day 28/homework/hw02.py
@@ -0,0 +1,8 @@
+# 4)შექმენით 6 ელემენტიანი tuple და გამოიყენეთ .index() და .count() მეთოდები
+
+names = ("Giorgi", "Sandro", "Mate", "Zura", "Otar", "Nika")
+
+print(names.index("Zura"))
+print(names.count("Sandro"))
+
+
